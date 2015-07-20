26e01a1e1c92c58d0a6406ddf40fa82912eb9324
Sagar <sag.sahay@gmail.com>
2015-07-20 23:40:53 +0100

    Trying to create a slider

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ app.directive("megaVideo", function ($sce) {
         restrict: 'E',
         templateUrl: 'megaVideo-template.html',
         scope: true,
+        transclude: true,
         link: function (scope, element, attrs) {
-            var videoPlayer = element.find('video')[0];
             scope.sources = [];
             function processSources() {
                 var sourceTypes = {
                     webm: {type: 'video/webm'},
                     mp4: {type: 'video/mp4'},
                     ogg: {type: 'video/ogg'}
-                };
+                }
                 for (source in sourceTypes) {
                     if (attrs.hasOwnProperty(source)) {
                         scope.sources.push(
@@ -28,29 +28,65 @@ app.directive("megaVideo", function ($sce) {
                     }
                 }
             }
-
             processSources();
-            scope.video = {
+        },
+        controller: function ($scope, $element, $attrs) {
+            console.log('controller function running');
+            var videoPlayer = $element.find('video')[0];
+            $scope.video = {
                 play: function () {
                     videoPlayer.play();
-                    scope.video.status = 'play';
+                    $scope.video.status = 'play';
                 },
                 pause: function () {
                     videoPlayer.pause();
-                    scope.video.status = 'pause';
+                    $scope.video.status = 'pause';
                 },
                 stop: function () {
                     videoPlayer.pause();
                     videoPlayer.currentTime = 0;
-                    scope.video.status = 'stop';
+                    $scope.video.status = 'stop';
                 },
                 togglePlay: function () {
-                    scope.video.status == 'play' ? this.pause() : this.play();
+                    $scope.video.status == 'play' ? this.pause() : this.play();
                 },
-                width: attrs.width,
-                height: attrs.height
+                width: $attrs.width,
+                height: $attrs.height
 
             };
+            var ctrl = this;
+            this.setVolume = function (level) {
+                videoPlayer.volume = level;
+            }
+        }
+
+    }
+});
+
+app.directive('volumeSlider', function () {
+    return {
+        require: '?^megaVideo',
+        restrict: 'A',
+        link: function (scope, element, attrs, megaVideoController) {
+            var initialVolume = parseFloat(attrs.initialVolume);
+            megaVideoController.setVolume(initialVolume);
+            angular.element(element.slider({
+                min: 0,
+                max: 1,
+                step: 0.01,
+                value: initialVolume,
+                orientation: "horizontal",
+                slide: function (event, ui) {
+                    scope.$apply(function () {
+                        megaVideoController.setVolume(ui.value);
+                    })
+                },
+                change: function (event, ui) {
+                    scope.$apply(function () {
+                        megaVideoController.setVolume(ui.value);
+                    })
+                }
+            }));
         }
     }
 });
@@ -61,4 +97,12 @@ app.directive("makeEditable", function () {
         templateUrl: 'makeEditable-template.html',
         transclude: true,
     }
+});
+
+app.controller("directiveController", function ($scope) {
+    var vm = this;
+    vm.isEditable = false;
+    vm.makeEditable = function () {
+        vm.isEditable = !vm.isEditable;
+    };
 });
